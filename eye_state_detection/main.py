36365620984bb6c37ddae3c71188c0deb09a6311
Krish <gupt1342@purdue.edu>
2026-08-01 19:56:25 +0000

    setup

---
+++ eye_state_detection/main.py
@@ -0,0 +1,21 @@
+# EEG Eye State Detection
+# Following: Purdue IEEE SMC Software Onboarding F25 doc
+
+# TODO 1: Imports
+# pandas, pathlib.Path, matplotlib.pyplot (see setup_test.py for import style)
+
+
+# TODO 2: Load Data
+# Use Path("data").iterdir() and pd.read_csv() to load all 5 CSVs into a list of DataFrames
+
+
+# TODO 3: View Data
+# Use .head() on one of the DataFrames to inspect columns
+
+
+def main():
+    pass
+
+
+if __name__ == "__main__":
+    main()
